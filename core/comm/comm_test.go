ffcbc306fce6e58bbf2c18806dba90dbe0c65755
Heysion Yuan <heysion@deepin.com>
2022-09-05 17:10:28 +0800

    fix: add GetFileSha256 test case
    add GetFileSha256 test case
    
    Log:

--- core/comm/comm_test.go
+++ core/comm/comm_test.go
@@ -10,6 +10,9 @@
 package comm
 
 import (
+	"io/ioutil"
+	"log"
+	"os"
 	"testing"
 )
 
@@ -17,5 +20,23 @@ import (
 func TestExecAndWait(t *testing.T) {
 	if v1, v2, err := ExecAndWait(10, "ls", "-al"); err != nil {
 		t.Error(err, v1, v2)
+	}
+}
+
+// GetFileSha256
+func TestGetFileSha256(t *testing.T) {
+	oneFileSha256 := "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b"
+	file, err := ioutil.TempFile("/tmp/", "sha256_")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	file.WriteString("1")
+
+	file.Close()
+
+	if fileSha256, err := GetFileSha256(file.Name()); fileSha256 != oneFileSha256 || err != nil {
+		t.Error("failed: ", err, file.Name())
 	}
 }
