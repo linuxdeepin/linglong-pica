b9e60dded5551b3dff1676fb69d90db94a993940
liujianqiang <liujianqiang@uniontech.com>
2022-11-21 11:04:40 +0800

    fix: 适配go 5.15版本编译
    适配go 5.15版本编译.
    
    Log:
    Change-Id: I4e39aec928268e835d6c369a7f6bad1aa3cf5c15

--- vendor/src/go.uber.org/zap/zapcore/console_encoder.go
+++ vendor/src/go.uber.org/zap/zapcore/console_encoder.go
@@ -56,7 +56,7 @@ type consoleEncoder struct {
 // encoder configuration, it will omit any element whose key is set to the empty
 // string.
 func NewConsoleEncoder(cfg EncoderConfig) Encoder {
-	if cfg.ConsoleSeparator == "" {
+	if len(cfg.ConsoleSeparator) == 0 {
 		// Use a default delimiter of '\t' for backwards compatibility
 		cfg.ConsoleSeparator = "\t"
 	}
@@ -125,7 +125,11 @@ func (c consoleEncoder) EncodeEntry(ent Entry, fields []Field) (*buffer.Buffer, 
 		line.AppendString(ent.Stack)
 	}
 
-	line.AppendString(c.LineEnding)
+	if c.LineEnding != "" {
+		line.AppendString(c.LineEnding)
+	} else {
+		line.AppendString(DefaultLineEnding)
+	}
 	return line, nil
 }
 
